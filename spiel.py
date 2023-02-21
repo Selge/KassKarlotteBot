5860c6612505225d9d0bc51d340ed5eb996ec1b1
Sergei Shcherbak <sshch1987@gmail.com>
2023-02-21 11:50:10 +0200

    aktualisiert

--- spiel.py
+++ spiel.py
@@ -8,7 +8,7 @@ import spielmaterial as sm
 
 bot = telebot.TeleBot(key.get_key())
 
-item = {}
+cell = {}
 gameIsStart = False
 game_won = False
 game_lost = False
@@ -16,6 +16,7 @@ game_lost = False
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
+    # keyboard
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     button = types.KeyboardButton("Start game")
     markup.add(button)
@@ -33,7 +34,7 @@ def mess(message):
             global gameIsStart
             gameIsStart = True
         else:
-            bot.send_message(message.chat.id, "use /start to play!")
+            bot.send_message(message.chat.id, "use /help for assist!")
 
     if gameIsStart == True:
         item = {}
@@ -41,7 +42,7 @@ def mess(message):
         global markup
         markup = types.InlineKeyboardMarkup(row_width=3)
         for i in range(0, 9):
-            item[i] = types.InlineKeyboardButton(sm.board[i], callback_data=str(i))
+            item[i] = types.InlineKeyboardButton(gameGround[i], callback_data=str(i))
         markup.row(item[0], item[1], item[2])
         markup.row(item[3], item[4], item[5])
         markup.row(item[6], item[7], item[8])
@@ -51,74 +52,175 @@ def mess(message):
 @bot.callback_query_handler(func=lambda call: True)
 def callbackInline(call):
     if (call.message):
-        while True:
-            the_board = [' '] * 10
-            game_is_active = True
-            turn = 'Human'
+        randomCell = random.randint(0, 9)
+        if gameGround[randomCell] == sm.player_sign:
+            randomCell = random.randint(0, 9)
+        if gameGround[randomCell] == sm.robot_sign:
+            randomCell = random.randint(0, 9)
+        if gameGround[randomCell] == " ":
+            gameGround[randomCell] = sm.robot_sign
 
-            while game_is_active:
-                if turn == 'Human':
-                    move = sm.get_player_move(the_board)
-                    sm.make_move(the_board, sm.player_sign, move)
+        for i in range(9):
+            if call.data == str(i):
+                if (gameGround[i] == " "):
+                    gameGround[i] = playerSymbol
 
-                    if sm.is_winner(the_board, sm.player_sign):
-                        sm.draw_board(the_board)
-                        print('Congrats! You have won the game!')
-                        game_is_active = False
-                    else:
-                        if sm.is_board_full(the_board):
-                            sm.draw_board(the_board)
-                            print('The game is a tie!')
-                            break
-                        else:
-                            turn = 'Computer'
+            sm.win(gameGround[0], gameGround[1], gameGround[2])
+            sm.win(gameGround[0], gameGround[4], gameGround[8])
+            sm.win(gameGround[2], gameGround[4], gameGround[6])
+            sm.win(gameGround[3], gameGround[4], gameGround[5])
+            sm.win(gameGround[6], gameGround[7], gameGround[8])
+            sm.win(gameGround[0], gameGround[3], gameGround[6])
+            sm.win(gameGround[1], gameGround[4], gameGround[7])
+            sm.win(gameGround[2], gameGround[5], gameGround[8])
+            sm.lose(gameGround[0], gameGround[1], gameGround[2])
+            sm.lose(gameGround[0], gameGround[4], gameGround[8])
+            sm.lose(gameGround[2], gameGround[4], gameGround[6])
+            sm.lose(gameGround[3], gameGround[4], gameGround[5])
+            sm.lose(gameGround[6], gameGround[7], gameGround[8])
+            sm.lose(gameGround[0], gameGround[3], gameGround[6])
+            sm.lose(gameGround[1], gameGround[4], gameGround[7])
+            sm.lose(gameGround[2], gameGround[5], gameGround[8])
 
-                else:
-                    move = sm.get_computer_move(the_board, sm.computer_sign)
-                    sm.make_move(the_board, sm.computer_sign, move)
-                    the_board[move] = sm.computer_sign
+            cell[i] = types.InlineKeyboardButton(gameGround[i], callback_data=str(i))
 
-                    if sm.is_winner(the_board, sm.player_sign):
-                        sm.draw_board(the_board)
-                        print('The computer has beaten you! You lose.')
-                        game_is_active = False
-                    else:
-                        if sm.is_board_full(the_board):
-                            sm.draw_board(the_board)
-                            print('The game is a tie!')
-                            break
-                        else:
-                            turn = 'Human'
-        #
-        # randomCell = sm.get_computer_move(sm.board, sm.computer_sign)
-        # if sm.board[randomCell] == sm.player_sign:
-        #     randomCell = random.randint(0, 9)
-        # if sm.board[randomCell] == sm.computer_sign:
-        #     randomCell = random.randint(0, 9)
-        # if sm.board[randomCell] == " ":
-        #     sm.board[randomCell] = sm.computer_sign
-
-        # update cells
         bot.edit_message_text(chat_id=call.message.chat.id,
                               message_id=call.message.message_id,
-                              text='Catch all rats!',
+                              text='Catch all the rats!',
                               reply_markup=None)
         global markup
-        markup.row(item[0], item[1], item[2])
-        markup.row(item[3], item[4], item[5])
-        markup.row(item[6], item[7], item[8])
+        markup.row(cell[0], cell[1], cell[2])
+        markup.row(cell[3], cell[4], cell[5])
+        markup.row(cell[6], cell[7], cell[8])
 
         bot.send_message(call.message.chat.id, "Choose a cell", reply_markup=markup)
         global game_won
         if game_won:
+            clear()
             bot.send_message(call.message.chat.id, "All the rats are in a new bag! 😸")
             game_won = True
             gameIsStart = False
         global game_lost
         if game_lost:
+            clear()
             bot.send_message(call.message.chat.id, "All the rats ran away! 😿")
             game_lost = True
             gameIsStart = False
 
 
 bot.polling(none_stop=True)
+
+
+
+
+# item = {}
+# gameIsStart = False
+# game_won = False
+# game_lost = False
+#
+#
+# @bot.message_handler(commands=['start'])
+# def welcome(message):
+#     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+#     button = types.KeyboardButton("Start game")
+#     markup.add(button)
+#
+
+#
+#
+# @bot.message_handler(content_types=['text'])
+# def mess(message):
+#     if message.chat.type == 'private':
+#         if message.text == "Start game":
+#             global gameIsStart
+#             gameIsStart = True
+#         else:
+#             bot.send_message(message.chat.id, "use /start to play!")
+#
+#     if gameIsStart == True:
+#         item = {}
+#         bot.send_message(message.chat.id, "The hunt is started")
+#         global markup
+#         markup = types.InlineKeyboardMarkup(row_width=3)
+#         for i in range(0, 9):
+#             item[i] = types.InlineKeyboardButton(sm.board[i], callback_data=str(i))
+#         markup.row(item[0], item[1], item[2])
+#         markup.row(item[3], item[4], item[5])
+#         markup.row(item[6], item[7], item[8])
+#         bot.send_message(message.chat.id, "Choose a cell", reply_markup=markup)
+#
+#
+# @bot.callback_query_handler(func=lambda call: True)
+# def callbackInline(call):
+#     if (call.message):
+#         while True:
+#             the_board = [' '] * 10
+#             game_is_active = True
+#             turn = 'Human'
+#
+#             while game_is_active:
+#                 if turn == 'Human':
+#                     move = sm.get_player_move(the_board)
+#                     sm.make_move(the_board, sm.player_sign, move)
+#
+#                     if sm.is_winner(the_board, sm.player_sign):
+#                         sm.draw_board(the_board)
+#                         print('Congrats! You have won the game!')
+#                         game_is_active = False
+#                     else:
+#                         if sm.is_board_full(the_board):
+#                             sm.draw_board(the_board)
+#                             print('The game is a tie!')
+#                             break
+#                         else:
+#                             turn = 'Computer'
+#
+#                 else:
+#                     move = sm.get_computer_move(the_board, sm.computer_sign)
+#                     sm.make_move(the_board, sm.computer_sign, move)
+#                     the_board[move] = sm.computer_sign
+#
+#                     if sm.is_winner(the_board, sm.player_sign):
+#                         sm.draw_board(the_board)
+#                         print('The computer has beaten you! You lose.')
+#                         game_is_active = False
+#                     else:
+#                         if sm.is_board_full(the_board):
+#                             sm.draw_board(the_board)
+#                             print('The game is a tie!')
+#                             break
+#                         else:
+#                             turn = 'Human'
+#         #
+#         # randomCell = sm.get_computer_move(sm.board, sm.computer_sign)
+#         # if sm.board[randomCell] == sm.player_sign:
+#         #     randomCell = random.randint(0, 9)
+#         # if sm.board[randomCell] == sm.computer_sign:
+#         #     randomCell = random.randint(0, 9)
+#         # if sm.board[randomCell] == " ":
+#         #     sm.board[randomCell] = sm.computer_sign
+#
+#         # update cells
+#         bot.edit_message_text(chat_id=call.message.chat.id,
+#                               message_id=call.message.message_id,
+#                               text='Catch all rats!',
+#                               reply_markup=None)
+#         global markup
+#         markup.row(item[0], item[1], item[2])
+#         markup.row(item[3], item[4], item[5])
+#         markup.row(item[6], item[7], item[8])
+#
+#         bot.send_message(call.message.chat.id, "Choose a cell", reply_markup=markup)
+#         global game_won
+#         if game_won:
+#             bot.send_message(call.message.chat.id, "All the rats are in a new bag! 😸")
+#             game_won = True
+#             gameIsStart = False
+#         global game_lost
+#         if game_lost:
+#             bot.send_message(call.message.chat.id, "All the rats ran away! 😿")
+#             game_lost = True
+#             gameIsStart = False
+#
+#
+# bot.polling(none_stop=True)
--- spielmaterial.py
+++ spielmaterial.py
@@ -1,81 +1,28 @@
-import random
-
-
-board = 10 * [" "]
+gameboard = 9 * [" "]
 player_sign = '🐱'
-computer_sign = '🐀'
-
-def make_move(board, sign, move):
-    board[move] = sign
+robot_sign = '🐀'
 
 
-def is_winner(board, sign):
-    return ((board[1] == sign and board[2] == sign and board[3] == sign) or
-            (board[4] == sign and board[5] == sign and board[6] == sign) or
-            (board[7] == sign and board[8] == sign and board[9] == sign) or
-            (board[1] == sign and board[4] == sign and board[7] == sign) or
-            (board[2] == sign and board[5] == sign and board[8] == sign) or
-            (board[3] == sign and board[6] == sign and board[9] == sign) or
-            (board[1] == sign and board[5] == sign and board[9] == sign) or
-            (board[3] == sign and board[5] == sign and board[7] == sign))
+def win(cell_1, cell_2, cell_3):
+    if cell_1 == player_sign and cell_2 == player_sign and cell_3 == player_sign:
+        global game_won
+        game_won = True
 
 
-def get_board_copy(board):
-    board_copy = []
-    for i in board:
-        board_copy.append(i)
-    return board_copy
+def lose(cell_1, cell_2, cell_3):
+    if cell_1 == robot_sign and cell_2 == robot_sign and cell_3 == robot_sign:
+        global losebool
+        game_lost = True
 
 
-def is_space_free(board, move):
-    return board[move] == ' '
+def defend(cell_1, cell_2, posDef):
+    if cell_1 == player_sign and cell_2 == player_sign:
+        posDef = robot_sign
 
 
-def choose_random_move_from_list(board, moves_list):
-    possible_moves = []
-    for i in moves_list:
-        if is_space_free(board, i):
-            possible_moves.append(i)
-
-    if len(possible_moves) != 0:
-        return random.choice(possible_moves)
-    else:
-        return None
-
-
-def get_computer_move(board, computer_sign):
-    even_moves = [2, 4, 6, 8]
-    odd_moves = [1, 3, 7, 9]
-
-    for i in range(1, 10):
-        board_copy = get_board_copy(board)
-        if is_space_free(board_copy, i):
-            make_move(board_copy, computer_sign, i)
-            if is_winner(board_copy, computer_sign):
-                return i
-
-    for i in range(1, 10):
-        board_copy = get_board_copy(board)
-        if is_space_free(board_copy, i):
-            make_move(board_copy, player_sign, i)
-            if is_winner(board_copy, player_sign):
-                return i
-
-    move = choose_random_move_from_list(board, odd_moves)
-    if move != None:
-        return move
-
-    if is_space_free(board, 5):
-        return 5
-
-    return choose_random_move_from_list(board, even_moves)
-
-
-def is_board_full(board):
-    for i in range(1, 10):
-        if is_space_free(board, i):
-            return False
-    return True
+def clear():
+    global gameboard
+    gameboard = 9 * [" "]
 
 
 def message():
